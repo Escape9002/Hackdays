5ca0a91e3a67c844679bc5ec3a23e86cd8ba42d6
Escape9002 <75317197+Escape9002@users.noreply.github.com>
2021-05-20 08:15:07 +0200

    Create Hackdays.ino

---
+++ Hackdays.ino
@@ -0,0 +1,18 @@
+/* HACKDAYS 2021 
+ *  Gruppe: Ole, Albert, Thilo, Antonio
+ *  
+ *  Sense of project:
+ *  
+ *  Wiring of Arduino:
+ *  
+ *  B-)
+ */
+
+void setup() {
+ 
+
+}
+
+void loop() {
+  
+}
