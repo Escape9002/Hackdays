71b99d4d7568f3820d64515f0db92c3917490c33
Escape9002 <75317197+Escape9002@users.noreply.github.com>
2021-05-20 10:33:23 +0200

    Create Overneegenerd_timer.ino

---
+++ Overneegenerd_timer/Overneegenerd_timer.ino
@@ -0,0 +1,92 @@
+/*
+ * Over engeneered timer!!!!
+ * 
+ * Button: D2
+ * Buzzer: D3
+ * LCD : I2C
+ */
+
+
+int Buzzer = 2;
+int interruptPin = 3;
+
+volatile boolean btnStatus = false;
+
+long timer;
+long timerset;
+int wait = 5000;
+int timerdif = 350;
+//-----------------------
+#include <Wire.h>
+#include "rgb_lcd.h"
+
+rgb_lcd lcd;
+
+const int colorR = 105;
+const int colorG = 75;
+const int colorB = 105;
+//-----------------------
+//----------------------------------------------SETUP
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(Buzzer, OUTPUT);
+
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), Btn, HIGH);
+  //-----------------------LCD
+  lcd.begin(16, 2);
+  lcd.setRGB(colorR, colorG, colorB);
+  lcd.setCursor(0, 0);
+  lcd.print("Loading....");
+  //-----------------------LCD
+  delay(1000);
+}
+//----------------------------------------------SETUP
+//----------------------------------------------LOOP
+void loop() {
+  timer = millis();
+  debug();
+  //-----------------------button
+  if (timer > timerset && btnStatus) {
+    digitalWrite(Buzzer, HIGH);
+    btnStatus = false;
+    delay(100);
+  } else {
+    digitalWrite(Buzzer, LOW);
+  }
+  //-----------------------button
+  //-----------------------lcd output
+  timerdif = clamp(0, wait, (timerset - timer));
+
+  //lcd.clear();
+  lcd.setCursor(0, 1);
+  lcd.print(timerdif);
+  //-----------------------lcd output
+}
+//----------------------------------------------LOOP
+//----------------------------------------------Interrupt function
+void Btn() {
+  lcd.clear();
+  btnStatus = true;
+  timerset = timer + wait;
+}
+//----------------------------------------------Interrupt function
+//----------------------------------------------debug
+void debug() {
+  String info = String(btnStatus) + " | " + String(timerdif);
+  Serial.println(info);
+}
+//----------------------------------------------debug
+//----------------------------------------------clamp
+int clamp(int min, int max, int var) {
+
+  if (var < min) {
+    var = min;
+  } else if (var > max) {
+    var = max;
+  }
+
+  return var;
+}
+//----------------------------------------------clamp
