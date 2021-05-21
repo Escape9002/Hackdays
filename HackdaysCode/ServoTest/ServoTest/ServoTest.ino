cab86062e0e1b23829f1c455115cd1ad5f7d1432
Escape9002 <75317197+Escape9002@users.noreply.github.com>
2021-05-21 10:51:07 +0200

    structure| test code

---
+++ HackdaysCode/ServoTest/ServoTest/ServoTest.ino
@@ -0,0 +1,58 @@
+#include <Servo.h>
+
+Servo servoA1;
+Servo servoA2;
+Servo servoA3;
+
+Servo servoB1;
+Servo servoB2;
+Servo servoB3;
+
+Servo servoGrnA1;
+Servo servoGrnA2;
+Servo servoGrnA3;
+
+Servo servoGrnB1;
+Servo servoGrnB2;
+Servo servoGrnB3;
+
+int Hhigh = 0;
+int SBackwards = -45;
+
+
+setup() {
+  //------------------Servo
+  servoA1.attach(13);
+  servoA2.attach(10);
+  servoA3.attach(9);
+
+  servoB1.attach(12);
+  servoB2.attach(5);
+  servoB3.attach(6);
+
+  servoGrnA1.attach(7);
+  servoGrnA2.attach(4);
+  servoGrnA3.attach(3);
+
+  servoGrnB1.attach(6);
+  servoGrnB1.attach(5);
+  servoGrnB1.attach(2);
+}
+
+loop() {
+  servoGrnA1.write(Hhigh); //hoch
+  servoGrnA2.write(Hhigh);
+  servoGrnA3.write(Hhigh);
+
+  servoB1.write(SBackwards);  //Rückwärts
+  servoB2.write(SBackwards);
+  servoB3.write(SBackwards);
+
+  servoGrnB1.write(Hhigh); //hoch
+  servoGrnB2.write(Hhigh);
+  servoGrnB3.write(Hhigh);
+
+  servoGrnB1.write(Hhigh); //hoch
+  servoGrnB2.write(Hhigh);
+  servoGrnB3.write(Hhigh);
+}
